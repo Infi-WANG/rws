f0049c2f416c565d4c600414bc86bd4d5250b2ee
Infi-WANG <69105313+Infi-WANG@users.noreply.github.com>
2021-04-12 15:36:05 +0800

    Add files via upload

---
+++ src/secrets.py
@@ -0,0 +1,14 @@
+# The Google Client ID for your application, used for Google Sign-in.
+# See https://developers.google.com/identity/sign-in/web/devconsole-project
+GOOGLE_CLIENT_ID = '544422738518-r6qf5vkiitnvu21n19choe8qqefbkoad.apps.googleusercontent.com'
+
+# A launch file to bring up some nodes.
+BRINGUP_FILE = '/home/infi/rws_ws/src/rws/launch/rws.launch'
+
+# The catkin workspace to load apps from.
+CATKIN_WS = '/home/infi/rws_ws/src/'
+
+# When developing, we use "gulp serve" to serve the frontend from localhost:5000.
+# If you are serving the frontend on a different host/port, change it here.
+# This is used to enable cross-origin resource sharing in development.
+DEV_FRONTEND_ORIGIN = 'http://localhost:5000'
